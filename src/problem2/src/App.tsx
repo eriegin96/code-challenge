1f5468244cd16a3ea26ff1fdae8774b0fe9efe96
eriegin <eriegin96@gmail.com>
2025-02-04 21:50:07 +0700

    feat: problem 2 set up source

---
+++ src/problem2/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl">App</div>
+      <Button variant="outline">Click me</Button>
+    </>
+  );
+}
+
+export default App;
